485f5cea83c3140087c67ef0e5672506605345d8
Jos <youssefelajbari@gmail.com>
2025-03-13 11:26:24 +0000

    Update InstallChrome.sh

--- InstallChrome.sh
+++ InstallChrome.sh
@@ -3,3 +3,4 @@ flatpak install -y flathub com.google.Chrome
 echo '#!/bin/bash' > browser.sh
 echo 'flatpak run com.google.Chrome' >> browser.sh
 chmod +x browser.sh
+xdg-settings set default-web-browser com.google.Chrome.desktop
