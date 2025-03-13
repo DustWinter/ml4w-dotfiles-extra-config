3e6f2796c8806775b3febe8038831bdb86064152
Jos <youssefelajbari@gmail.com>
2025-03-13 11:30:50 +0000

    Update InstallChrome.sh

--- InstallChrome.sh
+++ InstallChrome.sh
@@ -2,5 +2,5 @@
 flatpak install -y flathub com.google.Chrome
 echo '#!/bin/bash' > ~/.config/ml4w/settings/browser.sh
 echo 'flatpak run com.google.Chrome' >> ~/.config/ml4w/settings/browser.sh
-chmod +x browser.sh
+chmod +x ~/.config/ml4w/settings/
 xdg-settings set default-web-browser com.google.Chrome.desktop
